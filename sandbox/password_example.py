5c6f083a35cf87a895882dbbd62006a0b09baae1
cgrime11 <71897695+cgrime11@users.noreply.github.com>
2021-09-22 22:03:04 -0400

    9/22 cont

--- sandbox/password_example.py
+++ sandbox/password_example.py
@@ -19,7 +19,8 @@ def generate_cred(password):
     salt=os.urandom(32)
     key= hashlib.pbkdf2_hmac("sha256", password.encode("utf-8"), salt, 100000)
     return{
-        "key":key
+        "key":key,
+        'salt':salt
     }
 
 def save_password(username,password):
